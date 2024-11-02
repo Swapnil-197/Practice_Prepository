50df17826ae9768bde66ca7222c9ca869bc30b35
Swapnil-197 <sspatil1907@gmail.com>
2024-11-02 11:40:51 +0530

    Initial commit

---
+++ src/main/java/in/learnjava/App.java
@@ -0,0 +1,29 @@
+package in.learnjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static void add() {
+		int a =10 ;
+		int b = 20 ;
+		int c = a + b;
+		
+		System.out.println("Addition of a & b is " + c );
+	}
+
+public static void sub() {
+		int a = 50 ;
+		int b = 20 ;
+		int c = a - b;
+		
+		System.out.println("Substaction of a & b is " + c );
+	}
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Swapnil!" );
+    }
+}
---
+++ src/main/java/in/learnjava/demo/EvenNumber.java
@@ -0,0 +1,17 @@
+package in.learnjava.demo;
+
+public class EvenNumber {
+
+	public static void main(String[] args) {
+		
+		int a = 11 ;
+		
+		if(a%2==0) {
+			System.out.println("Given number is even");
+		}else {
+			System.out.println("Given number is odd");
+		}
+
+	}
+
+}
